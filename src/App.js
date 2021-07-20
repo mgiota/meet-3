b68118ae8142b3e637b06694e7c4f832b15c6889
bfab11 <79426655+bfab11@users.noreply.github.com>
2021-07-19 21:00:46 -0700

    4.4 implemented code to pass tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import { extractLocations, getEvents } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-    </div>
-  );
+import './nprogress.css';
+
+class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;
